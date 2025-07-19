a8f389e51399a70f36ad5d28db4218499329070f
Abdo-Hussien <abdelrahman.hsharaf@gmail.com>
2025-07-19 03:15:29 +0300

    setting up api environment

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,7 +1,21 @@
 "use client";
 
 import DataTable from '@/components/ui/DataTable/data-table';
-export default function InvoicePage() {
+import { use, useEffect } from 'react';
+async function fetchProducts(query: string) {
+    const res = await fetch(`/api/products?q=${encodeURIComponent(query)}`);
+    const data = await res.json();
+    return data;
+}
+export default async function InvoicePage() {
+    useEffect(() => {
+        // This effect can be used to fetch products or perform other side effects
+        // when the component mounts.
+        fetchProducts("Apple").then(products => {
+            console.log(products);
+        })
+    }
+        , []);
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold mb-4">Invoice List</h1>
