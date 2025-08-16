ba0ff9f1e8d4a7145fa3dd43570173465ae0e471
Abdo-Hussien <abdelrahman.hsharaf@gmail.com>
2025-08-16 16:54:22 +0300

    loader for create invoice btn

--- src/app/invoice/InvoiceActions.tsx
+++ src/app/invoice/InvoiceActions.tsx
@@ -2,16 +2,20 @@
 
 import Button from "@/components/ui/button"
 import { InvoiceContext } from "@/store/invoice-context"
+import { Loader } from "@/components/ui/loader"
 import { useContext } from "react"
 
 const InvoiceActions = () => {
-    const { resetForm } = useContext(InvoiceContext)
-    
+    const { createInvoice, isCreatingInvoice, resetForm } = useContext(InvoiceContext)
     return (
         <div id="inv-main-actions">
             <div data-component="button-group" className="flex flex-wrap-reverse gap-4 align-top justify-end">
                 <Button onClick={resetForm} variant="outline">ابدأ من جديد</Button>
-                <Button onClick={() => { }}>إنشاء فاتورة</Button>
+                <Button onClick={createInvoice} disabled={isCreatingInvoice}>
+                    {isCreatingInvoice ? <><Loader size={16} color="white" /> &nbsp; جاري الإنشاء...</>
+                        : <>إنشاء فاتورة</>
+                    }
+                </Button>
             </div>
         </div>
     )
