64ede8c14e3802dff8a2d5a4497f1a71ebde0c98
Abdo-Hussien <abdelrahman.hsharaf@gmail.com>
2025-08-21 18:04:11 +0300

    standalone config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    output: 'standalone',
     webpack: (config, { isServer }) => {
         // Ignore .html files in problematic modules
         if (isServer) {
